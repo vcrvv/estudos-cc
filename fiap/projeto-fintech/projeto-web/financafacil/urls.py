62cc0f8bfd4c90975fce74f0f9e6c56442da8c78
vcrvv <135657437+vcrvv@users.noreply.github.com>
2024-05-22 15:37:36 -0400

    ..

--- fiap/projeto-fintech/projeto-web/financafacil/urls.py
+++ fiap/projeto-fintech/projeto-web/financafacil/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('logar', views.logar), # logar dps da barra carrega a funcao "logar" em views
-    path('registrar', views.registrar),
-    path('inicio', views.inicio)
+    path('inicio', views.inicio, name='inicio'),
+    path('logar', views.logar, name='logar'), # logar dps da barra carrega a funcao "logar" em views
+    path('registrar', views.registrar, name='registrar'),
+    path('get_name', views.get_name, name='get_name')
 ]
--- fiap/projeto-fintech/projeto-web/financafacil/views.py
+++ fiap/projeto-fintech/projeto-web/financafacil/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import NameForm
 
 
 def logar(request):
-    return render(request, 'entrar.html')
+    return render(request, './templates/entrar.html')
 
 
 def registrar(request):
-    return render(request, 'registrar.html')
+    return render(request, './templates/registrar.html')
 
 
 def inicio(request):
-    return render(request, 'inicial.html')
+    return render(request, './templates/inicial.html')
+
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/thanks/")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, "./templates/name.html", {"form": form})
