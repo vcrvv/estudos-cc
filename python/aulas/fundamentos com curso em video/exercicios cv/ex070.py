717b54dc6100d6972c66672da3f7504f987fc57f
vcrvv <135657437+vcrvv@users.noreply.github.com>
2024-06-14 13:35:49 -0400

    .

--- python/aulas/fundamentos com curso em video/exercicios cv/ex070.py
+++ python/aulas/fundamentos com curso em video/exercicios cv/ex070.py
@@ -1,3 +1,17 @@
 # simulador de compras
 # digite o nome e o preço do produto
 # retorne o valor total, qnts produtos passram de 1000 reais e o nome e o valor do produto mais barato
+
+print('Compras!')
+opcao = ''
+produtos = {}
+
+while opcao != 'N':
+    nome_produto = input('Digite o nome do produto: ')
+    preco_produto = float(input('Digite o valor do produto: '))
+
+    opcao = input('Quer continuar? [S/N]').upper()[0]
+    if opcao == 'N':
+        break
+    
+print('fim.')
