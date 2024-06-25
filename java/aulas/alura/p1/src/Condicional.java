030073f5af0f51762c686297dc48002a89a00f4e
vcrvv <135657437+vcrvv@users.noreply.github.com>
2024-06-25 16:52:06 -0400

    iniciando java

--- java/aulas/alura/p1/src/Condicional.java
+++ java/aulas/alura/p1/src/Condicional.java
@@ -2,7 +2,6 @@ public class Condicional {
     public static void main(String[] args) {
       int anoDeLancamento = 2022;
       boolean incluidoNoPlano = true;
-      double notaDoFilme = 7.8;
       String tipoPlano = "plus";
 
 
--- java/aulas/alura/p1/src/Loop.java
+++ java/aulas/alura/p1/src/Loop.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class Loop {
     public static void main(String[] args) {
-        Scanner leitura = new Scanner(System.in);
-        double mediaAvaliacao = 0;
-        double nota = 0;
-
-        for (int i = 0; i < 3; i++) {
-            System.out.print("Diga a sua avaliação para o filme: ");
-            nota = leitura.nextDouble();
-            mediaAvaliacao += nota; 
+        try (Scanner leitura = new Scanner(System.in)) {
+            double mediaAvaliacao = 0;
+            double nota = 0;
+            
+            for (int i = 0; i < 3; i++) {
+                System.out.print("Diga a sua avaliação para o filme: ");
+                nota = leitura.nextDouble();
+                mediaAvaliacao += nota; 
+            }
+            
+            System.out.println("A avaliação média é %.1f".formatted(mediaAvaliacao / 3));
         }
-
-        System.out.println("A avaliação média é %.1f".formatted(mediaAvaliacao / 3));
-
-        leitura.close();
     }
 }
