e1607f28c596dca21d91155e8f0d4a5246287718
vini <viniciusoliveira09_@Outlook.com>
2024-07-15 10:17:30 -0400

    .

--- python/teste.py
+++ python/teste.py
@@ -1,9 +1,1 @@
-paises = []
-print("oi")
-quantidade = int(input("Quantos quer adicionar? "))
-
-for n in range(quantidade):
-    pais = str(input(""))
-    paises.add(pais)
-    
-print(quantidade)       
+print('oiiii')
