3238514f58b3e366e4aa666f2103a04210ecea3a
vini <viniciusoliveira09_@Outlook.com>
2024-08-16 14:16:07 -0400

    yo

--- fiap/projeto-fintech/projeto-web/financafacil/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('inicio', views.inicio, name='inicio'),
-    path('logar', views.logar, name='logar'), # logar dps da barra carrega a funcao "logar" em views
-    path('registrar', views.registrar, name='registrar'),
-    path('get_name', views.get_name, name='get_name')
-]
