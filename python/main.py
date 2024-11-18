eb36db6e82550fc4db3a84f7cf17634a711518f9
vini <viniciusoliveira09_@Outlook.com>
2024-11-18 07:38:35 -0400

    ....

--- python/main.py
+++ python/main.py
@@ -2,12 +2,11 @@
 nums = [7,12,143,200,45,32]
 target = 232
 result = []
+
 for num in nums:
     for num2 in nums:
         if num + num2 == target and num != num2:
-
-                result.append(nums.index(num))
-                result.append(nums.index(num2))
+            result.append(nums.index(num))
+            result.append(nums.index(num2))
                 
-
 print(result)
