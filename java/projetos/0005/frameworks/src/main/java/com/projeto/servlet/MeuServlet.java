ab844eaf1b793776d4b402e4828326c6c1a60055
vini <viniciusoliveira09_@Outlook.com>
2025-01-09 14:35:20 -0400

    bbbbb

--- java/projetos/0005/frameworks/src/main/java/com/projeto/servlet/MeuServlet.java
+++ java/projetos/0005/frameworks/src/main/java/com/projeto/servlet/MeuServlet.java
@@ -1,12 +1,11 @@
 package com.projeto.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
