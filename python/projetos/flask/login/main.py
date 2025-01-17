257c5b7658060da019e9efbddb103835b8c66938
vini <viniciusoliveira09_@Outlook.com>
2025-01-17 11:34:16 -0400

    Adicionando algumas validações

--- python/projetos/flask/login/main.py
+++ python/projetos/flask/login/main.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py') 
 
 db = SQLAlchemy(app)   
 csrf = CSRFProtect(app)
+bc = Bcrypt(app)
 
 from routes import *
 from models import *
--- python/projetos/flask/login/routes.py
+++ python/projetos/flask/login/routes.py
@@ -1,19 +1,8 @@
-from flask import request, session, render_template, redirect, url_for
-from main import app, db
-from models import User
+from flask import request, session, render_template, redirect, url_for, flash
+from main import app, db, bc
+from models import User 
 
 # Criar um auth_routes separado para atenticações
-
-# validar dados no cadastro
-# senha forte
-# encriptar senha antes de add no banco
-# validar email
-
-# autenticação de dados no login
-# comparar senha do formulario encriptado com a senha no banco de dados
-# verificar se conta existe
-# mostrar mensagens de erro no login e no cadastro
-
 # redefinir senha com email codigo enviado por email
 
 @app.route('/', methods=['GET'])
@@ -24,18 +13,42 @@ def home():
 def register():
     if request.method == 'POST':
         user = request.form
+        
+        username = user['username']
+        email = user['email']
+        password = user['password']
+        confirmPassword = user['confirmPassword']
+        
+        if not all([username, email, password, confirmPassword]):
+            flash('Todos os campos são obrigatórios')
+            return redirect(url_for('register'))
+        
+        if User.query.filter_by(username = username).first():
+            flash("Nome de usuario já cadastrado.")
+            return redirect(url_for('register'))
+        
+        if User.query.filter_by(email=email).first():
+            flash("Email já cadastrado.")
+            return redirect(url_for('register'))
+                 
+        if password != confirmPassword:        
+            flash("Senhas incompatíveis")
+            return redirect(url_for('register'))
+        
         new_user = User(
-            username = user['username'],
-            email = user['email'],
-            password = user['password']
-        )
+            username = username,
+            email = email,
+            password = bc.generate_password_hash(password))
+        
         db.session.add(new_user)
         db.session.commit()
         
-        session['username'] = user['username']
+        session['username'] = username
         
+        flash("Conta criada com sucesso!", 'success')
         return redirect(url_for('home'))
-              
+                
+                     
     return render_template('register.html')
 
 
@@ -43,17 +56,24 @@ def register():
 def login():
     if request.method == 'POST':
         form = request.form
+        
         user = User.query.filter_by(email = form['email']).first()
-        print(user)
         
-        if user.password == form['password']:
-            session['username'] = user.username
-            return redirect(url_for('home'))
-        
+        if user:
+            if bc.check_password_hash(user.password, form['password']):
+                session['username'] = user.username
+                flash('Login realizado com sucesso! :)', "success")
+                return redirect(url_for('home'))
+            
+            flash("Senha incorreta.")
+        else:
+            flash("Email não encontrado.")
+            
     return render_template('login.html')
 
 
 @app.route('/logout')
 def logout():
-    session['username'] = None
+    session.clear()
+    flash("Deslogou com sucesso.", "success")    
     return redirect(url_for('home')) 
