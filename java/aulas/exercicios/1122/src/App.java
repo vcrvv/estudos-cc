019e42f88eb38753c1cb6549d44e7fd2af868341
vini <viniciusoliveira09_@Outlook.com>
2025-01-20 13:45:14 -0400

    ....

--- java/aulas/exercicios/1122/src/App.java
+++ java/aulas/exercicios/1122/src/App.java
@@ -18,6 +18,23 @@ public class App {
             System.out.println("[" + i + "] = " + vetor[i]);
         }   
         
+        // Notas do aluno, cada elemento representa uma disciplina
+        double[] notasAluno = {7.5, 8.0, 9.0, 6.5, 8.0};
+
+        // Calculando a média das notas
+        double somaNotas = 0;
+        for (int i = 0; i < notasAluno.length ; i++){
+            somaNotas += notasAluno[i];
+        }
+        double media = somaNotas / notasAluno.length;
+
+        // Imprimindo a média e determinando a situação do aluno
+        System.out.println("Média do aluno: " + media);
+        if(media >= 7.0) {
+            System.out.println("Aluno aprovado!");
+        } else {
+            System.out.println("Aluno reprovado.");
+        }
     }
 }
 
